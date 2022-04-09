79912ae00143cb7ae9702d823b4d7e2c24e9ba3f
Сафин Руслан Мухсиддинович <pl8426@pl1.edu>
2022-04-09 15:57:43 +0500

    save

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/go-pg/pg/v10"
+)
+
+func pgDataBase() (con *pg.DB) {
+	address := fmt.Sprintf("%s:%s", "localhost", "5432")
+	options := &pg.Options{
+		User: "postgres",
+		Password: "",
+		Addr: address,
+		Database: "notice",
+		PoolSize: 50,
+
+	}
+
+	con = pg.Connect(options)
+	if con == nil {
+		log.Fatalf("Не удалось подключиться к postgress")
+	}
+	return
+}
+
+func Api(c *gin.Context) {
+	db := pgDataBase()
+
+	db.Close()
+
+	c.JSON(200, gin.H{
+		"api": "notice",
+
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/api", Api)
+	r.Run("0.0.0.0:9090")
+}
