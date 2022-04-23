09395ab03e440eca1babea523e8372aef689fe3c
Rxckhar <russaf007@mail.ru>
2022-04-23 12:14:28 +0500

    save

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
---CREATE DATABASE notice OWNER postgres;--
-CREATE TABLE notes (
+-- CREATE DATABASE notice OWNER postgres;
+CREATE TABLE notes(
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     title TEXT,
-    info TEXT
+    info TEXT 
 )
